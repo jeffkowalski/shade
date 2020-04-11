96a3430d524c198e7024c1a4f9c7c6cba51ba2a0
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-04-10 22:56:59 -0700

    primarily re-formatting

--- src/shade.cpp
+++ src/shade.cpp
@@ -7,66 +7,65 @@
 //#define INDICATOR LED_BUILTIN
 #define INDICATOR D4
 
-SomfyRemote somfy("remote1", 0x131478); // <- Change remote name and remote code here!
+SomfyRemote somfy("remote1", 0x131478);  // <- Change remote name and remote code here!
 
 class Blinker {
-  bool enabled;
-  const int led = D4;
-  unsigned long previousMillis;        // will store last time LED was updated
-  const unsigned long interval = 1000; // interval at which to blink (milliseconds)
+    bool                enabled;
+    int const           led = D4;
+    unsigned long       previousMillis;   // will store last time LED was updated
+    unsigned long const interval = 1000;  // interval at which to blink (milliseconds)
 
-public:
-  Blinker(bool enable) : enabled(enable), previousMillis(0) {
-    if (enabled)
-      pinMode(led, OUTPUT);
-  }
-
-  void blink() {
-    if (!enabled)
-      return;
-    digitalWrite(led, LOW);
-    delay(100);
-    digitalWrite(led, HIGH);
-    delay(100);
-  }
-
-  void update() {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-      // Serial.println("blink");
-      blink();
-      previousMillis = currentMillis;
+  public:
+    Blinker(bool enable) : enabled(enable), previousMillis(0) {
+        if (enabled)
+            pinMode(led, OUTPUT);
     }
-  }
+
+    void blink() {
+        if (!enabled)
+            return;
+        digitalWrite(led, LOW);
+        delay(100);
+        digitalWrite(led, HIGH);
+        delay(100);
+    }
+
+    void update() {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+            // Serial.println("blink");
+            blink();
+            previousMillis = currentMillis;
+        }
+    }
 } Blinker(false);
 
 void commandShade(String command) {
-  Blinker.blink();
-  Serial.println(command);
-  somfy.move(command);
-  EEPROM.commit();
-  if (command == "UP")               // off
-    digitalWrite(INDICATOR, HIGH); // ESP8266 builtin LED is "backwards" i.e. active LOW
-  else if (command == "DOWN")        // on
-    digitalWrite(INDICATOR, LOW);  // ESP8266 builtin LED is "backwards" i.e. active LOW
+    Blinker.blink();
+    Serial.println(command);
+    somfy.move(command);
+    EEPROM.commit();
+    if (command[0] == 'U')              // off
+        digitalWrite(INDICATOR, HIGH);  // ESP8266 builtin LED is "backwards" i.e. active LOW
+    else if (command[0] == 'D')         // on
+        digitalWrite(INDICATOR, LOW);   // ESP8266 builtin LED is "backwards" i.e. active LOW
 }
 
 //
 // This function will be called when Alexa hears "switch [on | off] 'shade'"
 //
 void onVoiceCommand(bool onoff) {
-  Serial.printf("onVoiceCommand %d\n", onoff);
+    Serial.printf("onVoiceCommand %d\n", onoff);
 
-  if (onoff)
-    commandShade("DOWN");
-  else
-    commandShade("UP");
+    if (onoff)
+        commandShade("DOWN");
+    else
+        commandShade("UP");
 }
 
-//
 bool getAlexaState() {
-  Serial.printf("getAlexaState %d\n", !digitalRead(INDICATOR));
-  return !digitalRead(INDICATOR);
+    Serial.printf("getAlexaState %d\n", !digitalRead(INDICATOR));
+    return !digitalRead(INDICATOR);
 }
 
 //
@@ -76,35 +75,34 @@ bool getAlexaState() {
 weenyMo w("shade", onVoiceCommand);
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(9600);
 
-  pinMode(INDICATOR, OUTPUT);
-  EEPROM.begin(EEPROM_SIZE);
+    pinMode(INDICATOR, OUTPUT);
+    EEPROM.begin(EEPROM_SIZE);
 
-  Blinker.blink();
+    Blinker.blink();
 
-  WiFi.begin(WIFI_SSID, WIFI_PSK);
-  WiFi.waitForConnectResult(); // so much neater than those stupid loops and dots
-  w.gotIPAddress();            // ready to roll...Tell Alexa to discover devices.
+    WiFi.begin(WIFI_SSID, WIFI_PSK);
+    WiFi.waitForConnectResult();  // so much neater than those stupid loops and dots
+    w.gotIPAddress();             // ready to roll...Tell Alexa to discover devices.
 
-  Blinker.blink();
-  Blinker.blink();
+    Blinker.blink();
+    Blinker.blink();
 
-  Serial.println("To program, long-press the program button on remote until blind goes up and down "
-                 "slightly, then send 'PROGRAM' command via serial monitor.");
-  Serial.println("Ready");
+    Serial.println("To program, long-press the program button on remote until blind goes up and down "
+                   "slightly, then send 'PROGRAM' command via serial monitor.");
+    Serial.println("Ready");
 }
 
 void loop() {
-  // check if input is available
-  if (Serial.available()) {
-    String command = Serial.readString();
-    command.trim();
-    // Serial.println("got command\n"); Serial.print("."); Serial.print(command);
-    // Serial.print(".\n");
-    if (command == "UP" || command == "DOWN" || command == "MY" || command == "PROGRAM")
-      commandShade(command);
-  }
+    // check if input is available
+    if (Serial.available()) {
+        String command = Serial.readString();
+        command.trim();
+        if (command[0] == 'U' || command[0] == 'D' || command[0] == 'M' || command[0] == 'P')
+            for (int i = 0; i < 10; ++i)
+                commandShade(command);
+    }
 
-  Blinker.update();
+    Blinker.update();
 }
